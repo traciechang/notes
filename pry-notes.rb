66dc3e1b8fafad6f8e3e9e588ada7a0596c95e65
Tracie <traciechang@hotmail.com>
2017-09-01 00:58:04 +0000

    add notes

---
+++ pry-notes.rb
@@ -0,0 +1,11 @@
+pry
+-if you want to test things in terminal one by one:
+->gem 'pry'
+->bundle install
+->require 'pry'
+->binding.pry
+->pry
+->load beer.rb
+
+if you want to test only one line:
+- ^: line # you want to run
