e4706579472ca04bc5e1d16e9ec9a41e4c2d5e9e
Tracie <traciechang@hotmail.com>
2017-09-05 22:11:33 +0000

    edit notes

--- ruby_notes.rb
+++ ruby_notes.rb
@@ -63,15 +63,3 @@
 # - write method so that whatever method you call, it will do same thing
 # - Metaprogramming
 # 	-> difficult to find and debug because method is not explicitly defined
-
-# pry
-# -if you want to test things in terminal one by one:
-# ->gem 'pry'
-# ->bundle install
-# ->require 'pry'
-# ->binding.pry
-# ->pry
-# ->load beer.rb
-
-# if you want to test only one line:
-# - ^: line # you want to run
