27b7c38b6d0e5aa4d53df6eec370eb6807669487
Tracie <traciechang@hotmail.com>
2017-09-19 18:45:40 +0000

    css notes

---
+++ css.rb
@@ -0,0 +1,15 @@
+# CSS 9/18/17
+
+# CSS: presentation layer
+# HTML: structure
+
+# avoid tags that have no meaning, such as <div> and <span>
+
+# think of everything as boxes
+
+# Chrome Developer Tools
+# - let's you see what changes would look like before implementing it. You can just make the changes here and then copying it to your real file
+# - you can also go through the page to go to the specific code. right click to Inspect
+# - New style rule: shows you style sheets going into the page right now
+# - Elements tab: where you'll be doing most of your editing
+# - Sources: get your code here
