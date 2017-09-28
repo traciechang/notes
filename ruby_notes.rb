38824c6f15c9128207dc4f60e293ec388b600790
Tracie <traciechang@hotmail.com>
2017-09-28 20:45:54 +0000

    mod notes

--- ruby_notes.rb
+++ ruby_notes.rb
@@ -6,55 +6,6 @@
 
 # - destructuring
 
-# git
-# - a way to save your progress
-# - commands:
-# - git init #when you want to create repo
-# - git status
-# - git diff (see what you added)
-# 	-> q to quite
-
-# # 	git add
-# 	-> then need to specify a file
-# 	-'-A' -> means add every file that has not yet been committed
-
-# #	git commit -m "this is my first commit"
-# # git log
-# # git commit --amend -m "your message here" #to change the message
-
-# ##############
-# ## try putting something in ruby_scratch.rb and save the file
-# ## now type git diff if you want to see what you added
-# ## git add -A
-# ## git status
-# ## git commit -m "add my first line"
-# ## git log
-# ## git push
-
-# #############
-# 1. on c9, created repository locally (like having a pic on your local machine)
-# 2. now push local repository to remote (github) (like putting on dropbox and access available anywhere)
-# 3. copy url and go back to c9 (need to tell it where to go)
-# 	# git remote
-# 	# git remote add origin [paste url]
-# 	# git remote (now u should have origin)
-# 	# git push
-# 	-> could not push since we created a readme initially on github
-# 	-> it wants u to update locally to what it looks like remotely first
-# 	# git pull origin master
-
-# * create on github first
-
-# ##################
-# git clone [url] (after you already create repo on git)
-
-# 3 basic steps:
-# add -A
-# commit -m ""
-# push
-
-
-
 # method_missing
 # - write method so that whatever method you call, it will do same thing
 # - Metaprogramming
