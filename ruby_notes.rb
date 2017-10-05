b57deb3b64795846571b6fcb725745cb67c02401
Tracie <traciechang@hotmail.com>
2017-10-05 11:05:21 -0700

    add notes

--- ruby_notes.rb
+++ ruby_notes.rb
@@ -10,3 +10,11 @@
 # - write method so that whatever method you call, it will do same thing
 # - Metaprogramming
 # 	-> difficult to find and debug because method is not explicitly defined
+
+# ******************************************
+
+# Terminal
+    # which
+    # -v  -> to see ruby version
+    # rm  -> won't let you remove directory
+    # rm -rf  -> to delete everything
