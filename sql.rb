d60545025ae95bc75af13b8741db8867ee0fad91
Tracie <traciechang@hotmail.com>
2017-10-12 13:10:49 -0700

    add sql notes

---
+++ sql.rb
@@ -0,0 +1,13 @@
+# RDBMS Intro 10/4/17
+
+# - Relational Model
+# - RDBMS allows you interact with data (example: SQLite3, Postgres)
+
+# - DDL (data definition language)
+# 	- describes structure of tables
+# 	- build tables
+# 	- Ex: CREATE, DROP
+
+# - DML (data manipulation language)
+# 	- add, edit, delete data in tables
+# 	- SELECT, INSERT, UPDATE, DELETE
