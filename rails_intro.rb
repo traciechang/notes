e1134a23e38a203ba21fb9331a847f0e83bf7e5f
Tracie <traciechang@hotmail.com>
2017-10-19 12:51:12 -0700

    add rails notes

--- rails_intro.rb
+++ rails_intro.rb
@@ -65,3 +65,33 @@
 # -> rails g migration AddColorToCats to change our cat table
 
 # - migrations are meant to change the format of the table, not individual rows
+
+# ******************************************************************
+# Models 10/16/17
+
+# - using rails to interact w/ content of our tables. It will give us object oriented way to interact
+
+# - In text editor, right click models folder and click New File and give it the singular version of your table (ex. cat.rb)
+# - name of class and name of file should be same
+
+# class Cat < ActiveRecord::Base
+# end
+
+# - to open rails console, go to terminal and type:
+# rails console -or- 
+# rails c
+
+# - you can create an instance locally (c = Cat.new) and set things we've defined (c.name = "sarah")
+# -> c.save | this will insert your instance into database
+# -> sarah = Cat.first | this gets the first row from the db and assigns those data to sarah
+
+# -> jeff = Cat.new(name: "Jeff", color: "green") | you can also pass in an options hash 
+# -> jeff.save
+# -> Cat.all | to see all your cats
+# -> Cat.last | to see last cat in db
+# -> jeff.destroy |removes jeff from db
+
+# -> Cat.find(1) | finds the cat with id # 1
+# -> Cat.find_by(name: "sarah") to find by something other than id
+
+# -> Cat.create(name: "Jeff", color: "orange") | this create an instance AND save to db
