9049429b1df42766b5d4a32532ed7c91db6e1372
Tracie <traciechang@hotmail.com>
2017-10-19 12:50:29 -0700

    add rails notes

--- rails_intro.rb
+++ rails_intro.rb
@@ -26,3 +26,42 @@
 # - lib: holds classes that we made
 
 # - test: where we write our tests
+
+# ******************************************************************
+# Migrations
+
+# **rake is from Rails 4, use rails now
+
+# - paper trail, or record of how database got to current state
+
+# terminal:
+# -> rails generate migration CreateCatsTable
+
+# - if you now go to db > migrate, you will see it
+
+# - to run migration:
+# -> rake db:migrate
+
+# - if you run into database "something" does not exist, go to database.yml file
+# -> rake db:create
+# -> rake db:migrate
+# - this should run and create your cats table
+# - how do we see that it really created it? go to db/schema.rb to check
+# - you won't see id, but it's added in
+
+# -> rails g migration CreateToys
+# - this should make a new migration called CreateToys
+# - recommended that you put timestamps on every one you create
+
+# -> rake db:migrate
+# - what happens if you forgot to add a column after you run migrate?
+# - if you try to add it and run migrate again, it won't do anything cuz it keeps track of what u already migrate
+# -> rake db:rollback
+# - it undo what we did, it deleted entire table. however, this will probably give u error if u did more than create a table
+# - now u can add your column and migrate again.
+# - if you had already pushed to github and production, you don't want to rollback (unless u are in development)
+# - instead, make a new migration w/ ur corrected stuff.
+
+# -> rails g migration AddColorToCats to change our cat table
+
+# - migrations are meant to change the format of the table, not individual rows
