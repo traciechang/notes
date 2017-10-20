71a927df934ea561fe76b47be611b6112f23c4b8
Tracie <traciechang@hotmail.com>
2017-10-20 10:15:28 -0700

    add css notes

--- css_reset.rb
+++ css_reset.rb
@@ -2,3 +2,11 @@ CSS Reset 10/16/17
 
 - your browser has built in style sheets
 - two schools of thought: 1) I should design everything, 2) Built off of defaults
+
+- to reset defaults, do your own:
+    - go thru and list out all the tags you have
+    <style>
+    
+        html, body, section, article, h1, h2, p {
+            # don't really want to put values in this section
+            # may want to include some other things in here, like box sizing. but these are pretty default ones u should memorize
