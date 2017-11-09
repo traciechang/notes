2bc4226cb5322904a51bbff840d8f3677842ef76
Tracie <traciechang@hotmail.com>
2017-11-08 16:07:47 -0800

    add notes

--- authentication.rb
+++ authentication.rb
@@ -1,14 +1,14 @@
-Authentication 11/4/17
+# Authentication 11/4/17
 
-1. Secure State
-Ex. You are buying on Amazon, and adding items to your cart. A URL to your cart could be: amazon.com/carts/412. This is not good because it exposes your cart ID
+# 1. Secure State
+# Ex. You are buying on Amazon, and adding items to your cart. A URL to your cart could be: amazon.com/carts/412. This is not good because it exposes your cart ID
 
-- we could give it a random string of numbers instead, like amazon.com/carts/823487239487 which would still point to cart 412. This is still bad becuase whoever has this link can get to your cart.
+# - we could give it a random string of numbers instead, like amazon.com/carts/823487239487 which would still point to cart 412. This is still bad becuase whoever has this link can get to your cart.
 
-- What can we do to not expose the cart in URL but still connect to our user?
-    - currently there is no way to do this w/ tools we have
-    - HTTP is stateless: there is no way between different requests to the server that the server remembers who you are. Each request is independent.
-    - We have a way to solve this: cookies
+# - What can we do to not expose the cart in URL but still connect to our user?
+#     - currently there is no way to do this w/ tools we have
+#     - HTTP is stateless: there is no way between different requests to the server that the server remembers who you are. Each request is independent.
+#     - We have a way to solve this: cookies
 
 2. Cookies
 - pieces (4kb) of data sent from website to browser. browser stores cookies, saved on client side
