4fc3240e15269beed453ba4f2011d742e33ad6dc
Tracie <traciechang@hotmail.com>
2017-11-22 11:31:25 -0800

    add js notes

--- rack.rb
+++ rack.rb
@@ -1,14 +1,14 @@
-Rack 11/17/17
+# Rack 11/17/17
 
-- middleware that exists between web server and web application
-- why can't they just communicate directly?
-    - Server recieves HTTP request, goes to Ruby which does some logic, sends back to server, and server sends out HTTP response
+# - middleware that exists between web server and web application
+# - why can't they just communicate directly?
+#     - Server recieves HTTP request, goes to Ruby which does some logic, sends back to server, and server sends out HTTP response
 
-    Ruby has many frameworks:
-    - rails, sinatra, camping, padrino
+#     Ruby has many frameworks:
+#     - rails, sinatra, camping, padrino
 
-    Also many servers:
-    - Webrick, puma, thin, unicorn
+#     Also many servers:
+#     - Webrick, puma, thin, unicorn
 
     - server's responsibility to parse string into something that ruby can use. every server does this differently
     - once framework does something w/ this, it returns back something to server, which could be different for every framework
