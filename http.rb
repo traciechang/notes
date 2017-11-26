0cdfaaf2de51ceafaca03aaea2a94528acdbfb12
Tracie <traciechang@hotmail.com>
2017-11-26 10:55:21 -0800

    add notes

--- http.rb
+++ http.rb
@@ -6,24 +6,24 @@
 
 # Protocol - based on agreed upon set of rules of how communication takes place
 
-- HTTP Request
-Ex: we are connected to a server of a site and you want to request all cats on this site. Because u are requesting, it's a GET request
-- 1st part of HTTP Request: HTTP Method
-Ex. GET
-- 2nd part of this request is: Path
-ex: /cats
-- last part is the version used to communicate
-ex: HTTP/1.1
-- Headers:
-- Host: mycoolsite.com (site I intended to go to)
-- Accept language.en (i want english)
+# - HTTP Request
+# Ex: we are connected to a server of a site and you want to request all cats on this site. Because u are requesting, it's a GET request
+# - 1st part of HTTP Request: HTTP Method
+# Ex. GET
+# - 2nd part of this request is: Path
+# ex: /cats
+# - last part is the version used to communicate
+# ex: HTTP/1.1
+# - Headers:
+# - Host: mycoolsite.com (site I intended to go to)
+# - Accept language.en (i want english)
 
-- HTTP Response
-- HTTP/1.1 
-- status code gives info on how successful this request was: 200 OK
-- Headers:
-    - content length: 32
-    - content-type text/html
+# - HTTP Response
+# - HTTP/1.1 
+# - status code gives info on how successful this request was: 200 OK
+# - Headers:
+#     - content length: 32
+#     - content-type text/html
 
-    <html>
-    </html>
+#     <html>
+#     </html>
