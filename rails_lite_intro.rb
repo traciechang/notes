8f7204bb3343bbe7c2aaf20057e999c0c8a09da9
Tracie <traciechang@hotmail.com>
2017-11-25 20:42:41 -0800

    add notes

--- rails_lite_intro.rb
+++ rails_lite_intro.rb
@@ -6,16 +6,16 @@
 # What it is: web application framework: software framework for web apps
 # - for ex, if you're building your own bank app, you don't want to write boring repetitive work that is the same for every app. You want to make the stuff that is unique to your app. The framework does that for you.
 
-What tools does it give to web dev?
-- When HTTP request comes thru, Rails provides you:
-    - default web server that receives this request (web brick)
-    - it then goes into Rails Proper:
-    - first hits Router (included module in Rails): it checks if anything matches in routes file (that you wrote)
-    - instantiate new instance in Controller and send the action specified in Router, also give you params:
-    3 sources of params:
-    1. request body
-    2. query string
-    3. url 
+# What tools does it give to web dev?
+# - When HTTP request comes thru, Rails provides you:
+#     - default web server that receives this request (web brick)
+#     - it then goes into Rails Proper:
+#     - first hits Router (included module in Rails): it checks if anything matches in routes file (that you wrote)
+#     - instantiate new instance in Controller and send the action specified in Router, also give you params:
+#     3 sources of params:
+#     1. request body
+#     2. query string
+#     3. url 
 
     - Remember that your conotroller inherits from Action Controller. Your UserController will call UserModel, it will then hit databse, and return back bunch of User objects (ruby objects that are instances of User Model). User Controller then will render View.
     - Your Model inherits from Active Record Base
