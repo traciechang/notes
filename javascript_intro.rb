00646392cd344e624ac32c22e3a4745208b49722
Tracie <traciechang@hotmail.com>
2017-11-27 11:33:28 -0800

    add notes

--- javascript_intro.rb
+++ javascript_intro.rb
@@ -7,29 +7,29 @@
 #     this.pos = pos;
 # }
 
-const curry = new MBAPlayer();
+# const curry = new MBAPlayer();
 
-- every function in js has a prototype property defined on it
+# - every function in js has a prototype property defined on it
 
-NBAPlayer.prototype.dunk = function(){};
+# NBAPlayer.prototype.dunk = function(){};
 
-# method style invocation
-curry.dunk(); <- how does it know it has dunk defined for curry?
+# # method style invocation
+# curry.dunk(); <- how does it know it has dunk defined for curry?
 
 
-curry.__proto__ === MBAPlayer.prototype
+# curry.__proto__ === MBAPlayer.prototype
 
-- js will first look to see if dunk is defined on curry, which it isn't. It will then look at curry's prototype, which is MBAPlayer, and see that it has dunk defined on it
+# - js will first look to see if dunk is defined on curry, which it isn't. It will then look at curry's prototype, which is MBAPlayer, and see that it has dunk defined on it
 
-- We could also do this:
-    function NBAPlayer(name, team, pos){
-        this.name = name;
-        this.team = team;
-        this.pos = pos;
-        this.dunk = function(){};
-    }
+# - We could also do this:
+#     function NBAPlayer(name, team, pos){
+#         this.name = name;
+#         this.team = team;
+#         this.pos = pos;
+#         this.dunk = function(){};
+#     }
 
-    however, we would be creating this function for every instance of MBAPlayer we create, which is not DRY
+#     however, we would be creating this function for every instance of MBAPlayer we create, which is not DRY
 
 
 ****************************************************
