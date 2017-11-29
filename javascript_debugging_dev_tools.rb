ea788610bcef3bd60cdaed91c9f477d26731dbdb
Tracie <traciechang@hotmail.com>
2017-11-29 10:48:05 -0800

    add notes

---
+++ javascript_debugging_dev_tools.rb
@@ -0,0 +1,8 @@
+JS Debugging in Dev Tools 11/28/17
+
+- write your js code, create html file and link to the js file.
+- open hmtl file in browser and open dev Tools
+> go to Sources
+> click on the line number to create a breakpoint, similar to putting debugger in ruby code
+> refresh the page and it will pause at breakpoint
+> u can inspect elements yourself in the Console. For ex, if you want to chekc value of certain variables, type it there.
