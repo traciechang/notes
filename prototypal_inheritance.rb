98b8bdf23cb0ef989c125f8475c3100235ed0252
Tracie <traciechang@hotmail.com>
2017-11-30 12:10:39 -0800

    add notes

--- prototypal_inheritance.rb
+++ prototypal_inheritance.rb
@@ -19,13 +19,13 @@
 # - now say you do d.toS();
 # - it's going to first look in the instance - not there. Then look at prototype - not there. Then it looks at what constructor of what made prototype of Dog function. What is a prototype? An object - its an instance of Object class.
 
-- When we make an instance of some object using a constructor function, it sets the __proto to whatever its prototype is
-- Let's say we have Animal class. Dog and Cat classes will inherit from Animal. How to we set Dog's proto to Animal'ls proto?
-    - Use surrogate w/ empty function and set:
-    Surrogate.prototype = Animal.prototype
-    Dog proto can then point to Surrogate's proto
+# - When we make an instance of some object using a constructor function, it sets the __proto to whatever its prototype is
+# - Let's say we have Animal class. Dog and Cat classes will inherit from Animal. How to we set Dog's proto to Animal'ls proto?
+#     - Use surrogate w/ empty function and set:
+#     Surrogate.prototype = Animal.prototype
+#     Dog proto can then point to Surrogate's proto
 
-*****************************************************
-Javascript in the Browser
+# *****************************************************
+# Javascript in the Browser
 
-- Webpack is a module bundler
+# - Webpack is a module bundler
