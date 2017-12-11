65a42cef86835d732eac036023a74fc146ab90be
Tracie <traciechang@hotmail.com>
2017-12-11 09:38:04 -0800

    add notes

--- ajax.rb
+++ ajax.rb
@@ -10,10 +10,10 @@
 
 # Ex: Instagram. When you keep scrolling, pictures keep popping up. You don't have to click button that says next 5 pictures. Javascript detects that you're at bottom of page and requests more.
 
-Why? if we can reduce amount of times server needs to load things, we can reduce times
+# Why? if we can reduce amount of times server needs to load things, we can reduce times
 
-How does this work?
-- 1st request we make to some page, we will still get HTML
-It will have script src tag that will give JS code to load. JS will start running in the background. JS will make requests when you hit bottom of page.
+# How does this work?
+# - 1st request we make to some page, we will still get HTML
+# It will have script src tag that will give JS code to load. JS will start running in the background. JS will make requests when you hit bottom of page.
 
-$.ajax({}) #pass in options hash
+# $.ajax({}) #pass in options hash
