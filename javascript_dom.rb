7378ee93733b70648ecfbe8107ab91546aafe176
Tracie <traciechang@hotmail.com>
2017-12-23 11:04:39 -0800

    add notes

---
+++ javascript_dom.rb
@@ -0,0 +1,29 @@
+12/21/17 by I
+
+var matches = document.querySelectorAll("div.note, div.alert");
+
+const people = [
+  {
+    firstName: 'tracie',
+    lastName: 'chang'
+  },
+  {
+    firstName: 'ian',
+    lastName: 'nguyen'
+  }
+]
+
+people.forEach((person) => {
+  console.log(person.lastName);
+
+  // before es5/6
+  person["lastName"]
+
+  // with es5/6
+  person.lastName
+
+  // destructure
+  { lastName }
+
+  // do something to the person object
+})
