21fc66bb5cf552db64be183d1ff3a47679a2e9bf
Tracie <traciechang@hotmail.com>
2018-01-02 14:12:34 -0800

    add notes

--- react_intro.rb
+++ react_intro.rb
@@ -4,7 +4,7 @@ React Intro 1/2/18
 
 # - ignore node_modules folder and bundle.js for github. We don't want to push these to github because they are huge and they can just be redone when downloaded.
 
-- a component is a js function that will return html
+# - a component is a js function that will return html
 
-- React Lifecycle methods
-    - these are auto invoked and auto bind "this"
+# - React Lifecycle methods
+#     - these are auto invoked and auto bind "this"
