86a9e36c8a250642b50782c0bfd791222c9e5642
Tracie <traciechang@hotmail.com>
2018-01-02 14:12:17 -0800

    add notes

--- react_intro.rb
+++ react_intro.rb
@@ -1,8 +1,8 @@
 React Intro 1/2/18
 
-- How does broswer read JSX? It gets transpiled into JS
+# - How does broswer read JSX? It gets transpiled into JS
 
-- ignore node_modules folder and bundle.js for github. We don't want to push these to github because they are huge and they can just be redone when downloaded.
+# - ignore node_modules folder and bundle.js for github. We don't want to push these to github because they are huge and they can just be redone when downloaded.
 
 - a component is a js function that will return html
 
