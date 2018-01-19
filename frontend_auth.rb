0cbe659ab30b0a661cdf6dfff9742ce6b86f81dc
Tracie <traciechang@hotmail.com>
2018-01-19 11:49:20 -0800

    add ui ux notes

---
+++ frontend_auth.rb
@@ -0,0 +1,52 @@
+Frontend Authentication: Intro 1/18/18
+
+- Rails Authentication
+    - user table holds ID, email, password digest, session token
+
+- Main diff between rails auth and auth via API
+    - instead of rails backend serving html views, rails backend only will receive info in form of json from frontend & serving info back as json to frontend
+
+- heart of rails auth will stay same. Includes:
+    - user model
+    - user controller
+    - session controller
+    - application controller (ex. login, logout, current user helper, etc)
+
+*****************************************
+
+Frontend Auth: API Authentication
+
+- starts when user sees form - fill in username, pw, submit button.
+- package in json and sent to backend. POST to /users
+    - will hit rails router
+    - call Create in controller
+    - controller will create new user w/ info passed in and attempt to save to db
+
+*****************************************
+
+Frontend Auth: Frontend Auth Pattern
+
+- API Utils
+    - user object needs to be sent to "/users" as a POST request
+
+- storing current_user
+    - how to store current_user in frontend? Store in state.
+    state: {
+        session: {
+            currentUser: pusheen, (starts out as null. changes when user logs in)
+            errors: "suername has already been taken, pw too short, etc"
+        }
+    }
+    - ex: on welcome page, check if (current_user) { display user's name } else { <h2></h2> }
+
+- restricting access
+    - ex. you have homepage of all cats. you only let logged in user to create new cat. you can prevent a unlogged user from submitting a form, but it's better to just not let them access the create cat page in the first place. So when they try to go, u can redirect them to homepage or login page.
+    - u can check is current_user, if no, redirect
+    - code should go to a lifecylce method before component mounts
+
+- boostrapping current_user
+    - packinging HTML, JS, CSS, and current_user info and giving it as soon as app loads
+    - prevent logged in user from ever seeing login page. takes them to homepage immediately
+    - how to do this? in HTML, in script tag, set window.current_user = value of current user in rails backend
+        
+
---
+++ ui_ux_intro.rb
@@ -0,0 +1,97 @@
+Intro to UI/UX 1/18/18
+
+- User Interface
+    - the saddle, stirrup, and reigns
+    - how user interacts w/ it
+- User experience
+    - the feeling u get riding horse
+    - good UX does not mean pretty
+
+- in interviews, you can say that you are:
+    - product-focused and human-focused
+
+**********************************************
+
+UX for Web
+- expect users to muddle thru website, not linear
+    - take advantage of convention
+        - expect search bar will search entire website
+        - when click on logo, takes u to homepage
+        - etc. we expect this because other websites do this as well
+
+    - create effective visual hierarchies
+        - should be able to tell what are titles, etc
+
+    - eliminate distractions
+        - no extraneous stuff. minimalist is good
+
+- make it mindless, have low expectations of your users
+- quality = "less thought needed"
+
+principles of UX design
+- keep simple
+- guide users to what u want
+- everything should be where u expeect
+- should do what u expect
+- user test to ensure these
+
+- recruiters will not take time to try to figure out how to navigate your site
+
+***********************************************
+
+UI Affordance and Signifiers
+
+perceived affordance - what the user thinks is an affordance
+signifier - a clue that an object has an affordance, influencing perceived affordance
+    - ex. button is designed that let user know that it can be clicked
+    - explicit
+    - pattern
+    - metaphor
+    - negative: absence of affordance
+        - ex: greyed out button
+    - lying signifier - false perceived affordance (bad)
+        - looks like a button but its not clickable
+
+***********************************************
+
+Designing to Look good
+
+- Layout
+    - add white space
+    - put headings close to texts that follows them
+
+    - Use Grids
+
+    - have text hierarchies (h1, h2, etc)
+
+- Colors
+    - use constrained pallete
+    - only use a couple Colors
+
+- Fonts
+    - easy to read
+    - set yourself apart by not using default Font
+    - google fonts are good and easy
+
+- Forms
+    - one column
+    - specify erros inline (near field, instead of all at top/bottom)
+
+- when in doubt, copy someone else (design wise)
+
+- Images
+    - don't host images yourself
+    - reduce image size
+    - images are great
+
+- make purpose of site immediately clear
+    - use logo and tagline that get across what the site does
+    - dont confuse recruiters
+     
+- accessibility
+    - add alt text to every image
+    - use semantic tags and labels
+    - dont have flashing stuff
+
+    - significant contrast between text and background
+    - adding an "accessible view" button that replaces less accessible typefaces
