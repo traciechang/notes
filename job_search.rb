3abbd240dff8791604af230b5c2fdc4be8bf1ae3
Tracie <traciechang@hotmail.com>
2018-08-06 13:34:42 -0700

    add job search

---
+++ job_search.rb
@@ -0,0 +1,13 @@
+# Steps
+# - submit cover letter and resume
+# - phone interview
+#     - research company in advance
+#     - ask who you'll be interviewing w/
+#     - send a thank u note that evening
+#     - follow up in 1 week
+# - Take-home problem
+#     - algorithm problem or request to build something. Get a review from mentor after
+# - On site interview
+#     - ask who you'll be interviewing w/ 
+#     - send thank u note that evening
+# - negotiations
