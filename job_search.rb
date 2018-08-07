53900d545c34914853d160068383d21fab6d3ca4
Tracie <traciechang@hotmail.com>
2018-08-07 15:10:52 -0700

    job search notes

--- job_search.rb
+++ job_search.rb
@@ -12,6 +12,8 @@
 #     - send thank u note that evening
 # - negotiations
 
+# **********************************************************************
+
 # 8/6/2018 Personal Pitch
 
 # - Connection
@@ -30,3 +32,49 @@
 
 # - Tell a story
 #         - have beginning, middle, end
+
+# **********************************************************************
+
+8/7/2018 Interview skills
+
+- keep answers concise (30-90 seconds)
+- use positive framework/words
+- tell a story - concrete details w/ start, middle, end
+
+* Commonly Asked Questions *
+-  Tell me about yourself?
+- Why do you want to work for our company?
+- Tell me about a time you had a conflict with a supervisor?
+- What is your greatest strength/weakness?
+- Tell me about one of your projects?
+- What have you been learning lately?
+- What are your 5 year plans?  
+- Do you have any questions for us?
+
+* Behavioral Questions *
+- Asks about the way you’ve handled projects in the past. 
+- May start with “Tell me about a time when…” or similar. 
+- Request for concrete examples from your past.  
+- Examples:
+    Tell me about a challenge you ran into on that project.
+    What did you like most/least about that project?  
+    Tell me about a time you had a conflict with a supervisor.
+- (Spend less time describing problem, and more time on action you took to resolve issues.)
+
+* Problem/Action/Result (beginning/middle/end)
+ - Problem: The situation that required support or intervention.
+ - Action: What you did to address the problem.
+ - Result: The improved situation produced through your actions.
+
+ Ex: Tell me about a time you had to 
+ take initiative to get something done.  
+ 
+ - Problem: "When I was at [RampUp], we needed to store a lot of data but we kept all the data on spreadsheets. There were so many, the sheets were almost impossible to keep updated. Finding basic numbers for a client would sometimes take 30 minutes."
+ -Action: "So I started researching better ways to store information, and found a number of database options online. I researched three different options, presented the pros and cons of each to my manager, and recommended Salesforce as the best fit for us."
+ - Result: "I worked with our office manager to get the system set up, and two months later, we not only had cleaner data that was easier to update, but were able to keep track of almost twice as many metrics as we were before."
+
+* Thoughtful questions to ask interviewer
+- What are your procedures for testing and releasing code?  
+- What are the company’s goals this year, and what problems are you running into that a new employee can help with?    
+- What problems have you run into while scaling the user base?  
+- Why did you pick Angular.js as your front-end framework?
