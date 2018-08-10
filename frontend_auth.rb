834f6a71b6dd02a9a569a81ed62fb5be0395594b
Tracie <traciechang@hotmail.com>
2018-08-09 19:45:18 -0700

    add notes

--- frontend_auth.rb
+++ frontend_auth.rb
@@ -39,14 +39,14 @@
 #     }
 #     - ex: on welcome page, check if (current_user) { display user's name } else { <h2></h2> }
 
-- restricting access
-    - ex. you have homepage of all cats. you only let logged in user to create new cat. you can prevent a unlogged user from submitting a form, but it's better to just not let them access the create cat page in the first place. So when they try to go, u can redirect them to homepage or login page.
-    - u can check is current_user, if no, redirect
-    - code should go to a lifecylce method before component mounts
+# - restricting access
+#     - ex. you have homepage of all cats. you only let logged in user to create new cat. you can prevent a unlogged user from submitting a form, but it's better to just not let them access the create cat page in the first place. So when they try to go, u can redirect them to homepage or login page.
+#     - u can check is current_user, if no, redirect
+#     - code should go to a lifecylce method before component mounts
 
-- boostrapping current_user
-    - packinging HTML, JS, CSS, and current_user info and giving it as soon as app loads
-    - prevent logged in user from ever seeing login page. takes them to homepage immediately
-    - how to do this? in HTML, in script tag, set window.current_user = value of current user in rails backend
+# - boostrapping current_user
+#     - packinging HTML, JS, CSS, and current_user info and giving it as soon as app loads
+#     - prevent logged in user from ever seeing login page. takes them to homepage immediately
+#     - how to do this? in HTML, in script tag, set window.current_user = value of current user in rails backend
         
 
