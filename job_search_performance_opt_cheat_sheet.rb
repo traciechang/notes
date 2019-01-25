21311052479d933467fc729a79f69848bd9ab8f5
Tracie <traciechang@hotmail.com>
2019-01-25 13:15:15 -0800

    add notes

--- job_search_performance_opt_cheat_sheet.rb
+++ job_search_performance_opt_cheat_sheet.rb
@@ -11,7 +11,7 @@
 
 # - Application layer
 
-Cache DB queries (see Russian Doll Caching)
+# Cache DB queries (see Russian Doll Caching)
 Use ActiveRecord counter cache
 Use a faster server (see Unicorn)
 Trade more space for less time in your code
