7a3ea102c899ead6a4d4f30bf1faa03bedd3fe90
Tracie <traciechang@hotmail.com>
2019-01-28 12:54:47 -0800

    add notes

--- job_search_performance_opt_cheat_sheet.rb
+++ job_search_performance_opt_cheat_sheet.rb
@@ -14,7 +14,7 @@
 # Cache DB queries (see Russian Doll Caching)
 # Use ActiveRecord counter cache
 # Use a faster server (see Unicorn)
-Trade more space for less time in your code
+# Trade more space for less time in your code
 Cache rendered views
 Avoid N+1 queries
 Paginate long results
