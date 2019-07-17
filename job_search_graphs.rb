53602f76502cb641ddd8b2d0b1468f6c99db4edf
Tracie <traciechang@hotmail.com>
2019-07-16 20:26:01 -0500

    add noteds

--- job_search_graphs.rb
+++ job_search_graphs.rb
@@ -3,7 +3,7 @@
 # Introduction
 
 # 1. Vertices (dots)
-2. Edges (lines) that connect the vertices.
+# 2. Edges (lines) that connect the vertices.
     - can add directions (if u do, every point must have directions)
     - can also have weights (can leave unweighted or must add weight to all edges)
 
